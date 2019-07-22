e72187c21667d8c78d8a35099017b10e6f54cd35
Chirag Jain <jainchg@gmail.com>
2019-07-22 14:23:35 +0530

    added reddit keys

---
+++ Scripts/redditKeys.py
@@ -0,0 +1,11 @@
+# Keys for Reddit API
+# Not meant for Redistribution
+
+secret = '7xPRMWumJ3VvHpEUjIHLA4DkOHs'
+script = 'FeVAZ4yonaIjgw'
+name = 'redditflair'
+username = 'jnchirag'
+password = 'Precog@2019'
+
+mongoDB_Username = 'Precog'
+mongoDB_pass = 'Div%401234'
