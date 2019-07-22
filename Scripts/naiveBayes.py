3acc0c74325131e422a4f56da4e8a7498355e9d4
Chirag Jain <jainchg@gmail.com>
2019-07-22 19:57:29 +0530

    Optimized code

--- Scripts/naiveBayes.py
+++ Scripts/naiveBayes.py
@@ -1,45 +1,25 @@
 from sklearn.naive_bayes import MultinomialNB, GaussianNB
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
-from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer, TfidfVectorizer
-import Scripts.analyzeText as dataset
-import pandas as pd
-import numpy as np
 from joblib import dump, load
 
-dataset.processText()
-mapp = dataset.subreddits
+nb = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('clf', MultinomialNB()),])   
 
-flairs = ['Political', 'Non-Political', 'AskIndia', 'Reddiquette', 'Science & Technology', 'Policy & Economy', 'Finance & Business', 'Entertainment', 'Sports', 'Food', 'Photography', 'AMA']
-
-topics_data = pd.DataFrame(mapp)
-
-feature_combine = topics_data["title"] + topics_data["comments"] + topics_data["url"]
-
-cat = topics_data['flair']
-V = topics_data['comments']
-
-x_train, x_test, y_train, y_test = train_test_split(V, cat, test_size=0.3, random_state=42)
-
-nb = Pipeline([('vect', CountVectorizer()),
-            ('tfidf', TfidfTransformer()),
-            ('clf', MultinomialNB()),
-            ])   
-
-def NBtrain():
-    global nb, x_train, y_train
+def NBtrain(x_train, x_test, y_train, y_test):
+    global nb
     nb.fit(x_train, y_train)
     dump(nb, 'naiveBayes.joblib') 
 
-# print(y_pred)
-# print('accuracy %s' % accuracy_score(y_pred, y_test))
-# print(classification_report(y_test, y_pred,target_names=flairs))
+def NBtrainResults(x_train, x_test, y_train, y_test, flairs):
+    global nb
+    nb.fit(x_train, y_train)
+    y_pred = nb.predict(x_test)
+    print('accuracy %s' % accuracy_score(y_pred, y_test))
+    print(classification_report(y_test, y_pred,target_names=flairs))
 
 def NBpredict(elem):
-    # global nb
-    # train()
     nb = load('naiveBayes.joblib') 
     x_test = [elem]
     y_pred = nb.predict(x_test)
--- trainModels.py
+++ trainModels.py
@@ -1,6 +1,21 @@
 from Scripts.naiveBayes import NBtrain
+from sklearn.model_selection import train_test_split
+import Scripts.analyzeText as dataset
+import pandas as pd
 
 def train():
-    NBtrain()
+    dataset.processText()
+    mapp = dataset.subreddits
+
+    flairs = ['Political', 'Non-Political', 'AskIndia', 'Reddiquette', 'Science & Technology', 'Policy & Economy', 'Finance & Business', 'Entertainment', 'Sports', 'Food', 'Photography', 'AMA']
+
+    topics_data = pd.DataFrame(mapp)
+
+    feature_combine = topics_data["title"] + topics_data["comments"] + topics_data["url"]
+
+    cat = topics_data['flair']
+    V = topics_data['comments']
+    x_train, x_test, y_train, y_test = train_test_split(V, cat, test_size=0.3, random_state=42)
+    NBtrain(x_train, x_test, y_train, y_test)
 
 train()
